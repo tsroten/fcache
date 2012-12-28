638e5c92059bb39542b74a3ea4b2f65460d84a03
Thomas Roten <thomas@roten.us>
2012-12-28 23:33:46 +0800

    Added remove/delete methods and filled in docstrings

--- fcache/fcache.py
+++ fcache/fcache.py
@@ -29,7 +29,6 @@ import datetime
 import hashlib
 import os
 import pickle
-import sys
 import tempfile
 
 import appdirs
@@ -44,9 +43,46 @@ class Cache(object):
     proper OS-designated (typically more of a convention) application cache
     directories.
 
+    Example usage:
+        import fcache
+        # create a cache file named "temperatures" for the "weather-cli" app.
+        current_weather = fcache.Cache("temperatures", "weather-cli",
+                                       "Joe Developer")
+        # create some data that needs to be cached.
+        boston_weather = {"temp": 64, "condition": "cloudy"}
+        # cache the weather data in the "boston" key for 1 hour.
+        current_weather.set("boston", boston_weather, 60 * 60)
+        # get the data from the cache file.
+        print current_weather.get("boston")["temp"]
+        # -> 64
+
+    Methods:
+        get: read some data from the cache file
+        set: store some data into the cache file
+        remove: delete a key from the cache file
+        delete: delete the cache file and all data in it.
+
+
     """
 
     def __init__(self, cachename, appname, appauthor=None):
+        """Setup a Cache object.
+
+        This method creates a cache file if there isn't one
+        already created for [cachename].
+
+        Args:
+            cachename: a unique name for this cache file.
+            appname: the name of this application -- used to determine the
+                correct cache directory to store files in.
+            appauthor: the name of this apps author or company -- used to
+                determine the correct cache directory, in Windows,  to store
+                files in.
+        Raises:
+            TypeError: 'appauthor' was not provided, but is needed
+                because the host system is running Windows.
+
+        """
         self.cachename = cachename
         try:
             self.cachedir = appdirs.user_cache_dir(appname, appauthor)
@@ -54,46 +90,92 @@ class Cache(object):
             raise TypeError("'user_cache_dir' expects 'appauthor' on Windows,"
                             "but received None")
         self.filename = os.path.join(self.cachedir,
-                                      hashlib.md5(cachename).hexdigest())
+                                     hashlib.md5(cachename).hexdigest())
         if os.access(self.filename, os.F_OK) is False:
             self._create()
 
     def get(self, name):
+        """Get data from the cache.
+
+        Args:
+            name: the key name of the data.
+        Returns:
+            data: the asked-for data; None if couldn't be retrieved.
+
+        """
         data = self._read()
-        if data is None:
+        if data is None or name not in data:
             return None
         if ((data[name]["expires"] is None) or
                 ((datetime.datetime.now() -
-                data[name]["expires"]).total_seconds() < 0)):
+                  data[name]["expires"]).total_seconds() < 0)):
             return data[name]["data"]
         else:
             return None
 
     def set(self, name, value, timeout=None):
+        """Store data in the cache.
+
+        Args:
+            name: the name given to the data; to be used for retrieval.
+            value: the data to be stored.
+            timeout: how long in seconds the data should be considered
+                valid -- defaults to forever.
+
+        """
         data = self._read()
         if data is None:
             data = {}
         if timeout is None:
             expires = None
         else:
-            expires = datetime.datetime.now() + datetime.timedelta(seconds=timeout)
+            expires = (datetime.datetime.now() +
+                       datetime.timedelta(seconds=timeout))
         data[name] = {"expires": expires, "data": value}
         self._write(data)
 
-
     def remove(self, name):
-        pass
+        """Remove data from the cache.
+
+        Args:
+            name: the key name of the data to be removed.
+        Returns:
+            boolean: whether or not the data could be found and deleted.
+
+        """
+        data = self._read()
+        try:
+            del data[name]
+        except KeyError:
+            return False
+        self._write(data)
+        return True
 
     def delete(self):
-        pass
+        """Delete the cache file.
+
+        NOTE: this removes all data from the cache.
+
+        Returns:
+            boolean: whether or not the file was found and successfully
+                deleted.
+
+        """
+        try:
+            os.remove(self.filename)
+        except:
+            return False
+        return True
 
     def _create(self):
+        """Create a cache file."""
         if os.access(self.cachedir, os.F_OK) is False:
             os.makedirs(self.cachedir)
         f, tmp = tempfile.mkstemp(dir=self.cachedir)
         os.rename(tmp, self.filename)
 
     def _read(self):
+        """Open a file and uses pickle to retrieve its data"""
         with open(self.filename, "rb") as f:
             try:
                 data = pickle.load(f)
@@ -102,5 +184,6 @@ class Cache(object):
         return data
 
     def _write(self, data):
+        """Use pickle to save data to a file"""
         with open(self.filename, "wb") as f:
             pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
