209124db79661ce5936c92b93f07f08bc702eb82
Thomas Roten <thomas@roten.us>
2012-12-29 13:33:59 +0800

    Switched to /lib structure and wrote setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
+from distutils.core import setup
 
+setup(
+    name = "fcache",
+    package_dir = {"": "lib"}
+    py_modules = ["fcache"]
+    version = "0.1",
+    description = "A simple file-based cache.",
+    author = "Thomas Roten",
+    author_email = "thomas@roten.us",
+    url = "http://github.com/tsroten",
+    download_url = "",
+    keywords = ["cache", "file"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        ],
+    long_description = """
+
+    """
+)
---
+++ test/testCache.py
@@ -0,0 +1,37 @@
+import os
+import time
+import unittest
+
+import lib.fcache
+
+
+class testCache(unittest.TestCase):
+
+    def setUp(self):
+        self.cache = lib.fcache.Cache("unittest", "fcache", "Thomas Roten")
+        self.cache.set("num", 43)
+        self.cache.set("timer", 1, 2)
+
+    def tearDown(self):
+        self.cache.delete()
+        self.cache = None
+
+    def test_init(self):
+        self.assertTrue(os.access(self.cache.filename, os.F_OK))
+
+    def test_get(self):
+        self.assertEqual(self.cache.get("num"), 43)
+        self.assertEqual(self.cache.get("alsdf"), None)
+        self.assertEqual(self.cache.get("timer"), 1)
+        time.sleep(2)
+        self.assertEqual(self.cache.get("timer"), None)
+
+    def test_remove(self):
+        self.assertTrue(self.cache.remove("num"))
+        self.assertEqual(self.cache.get("num"), None)
+        self.assertFalse(self.cache.remove("jlk"))
+
+    def test_delete(self):
+        self.assertTrue(self.cache.delete())
+        self.assertFalse(os.access(self.cache.filename, os.F_OK))
+        self.assertFalse(self.cache.delete())
